972edc4a69ff0a004d08481ca2a6dfcdbbcf7fa7
dstroobants <dstroobants@quantcast.com>
2022-01-24 14:10:39 +0000

    Refactor

--- main.go
+++ main.go
@@ -2,382 +2,120 @@ package main
 
 import (
 	"fmt"
-	"go-booking-app/helper"
+	"sync"
+	"time"
 )
 
-const conferenceTickets = 50
+const conferenceTickets int = 50
 
-var conferenceName = "Go conference"
+var conferenceName = "Go Conference"
 var remainingTickets uint = 50
-var bookings = []map[string]string{}
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
 
 func main() {
 
-	helper.GreetUsers(remainingTickets, conferenceName, conferenceTickets)
+	greetUsers()
 
-	for { // Infinite loop
+	// for {
+	firstName, lastName, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
-		firstName, lastName, email, userTickets := helper.GetUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(remainingTickets, firstName, lastName, email, userTickets)
+	if isValidName && isValidEmail && isValidTicketNumber {
 
-		if isValidName && isValidEmail && isValidTicketNumber { // If user tries to book more tickets than are available
+		bookTicket(userTickets, firstName, lastName, email)
 
-			bookings := helper.BookTicket(remainingTickets, userTickets, bookings, firstName, lastName, email, conferenceName)
-			firstNames := helper.GetFirstNames(bookings)
-			fmt.Printf("The first names in the bookings are: %v\n", firstNames)
+		wg.Add(1)
+		go sendTicket(userTickets, firstName, lastName, email)
 
-			if remainingTickets == 0 {
-				fmt.Println("Sorry, we are sold out")
-				break
-			}
-		} else {
-			switch {
-			case !isValidName:
-				fmt.Println("Please enter a valid name")
-			case !isValidEmail:
-				fmt.Println("Please enter a valid email")
-			case !isValidTicketNumber:
-				fmt.Println("Please enter a valid ticket number")
-			}
+		firstNames := getFirstNames()
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			// break
+		}
+	} else {
+		if !isValidName {
+			fmt.Println("first name or last name you entered is too short")
+		}
+		if !isValidEmail {
+			fmt.Println("email address you entered doesn't contain @ sign")
+		}
+		if !isValidTicketNumber {
+			fmt.Println("number of tickets you entered is invalid")
 		}
 	}
-	fmt.Println("Exiting booking application...")
-
+	//}
+	wg.Wait()
 }
 
-/*
-func greetUsers(confName string, confTickets int) {
-	fmt.Printf("Welcome to the %v booking application\n", confName)
-	fmt.Printf("There is a total of %v tickets, %v tickets are still available\n", confTickets,
-		remainingTickets)
-	fmt.Println("Get your tickets here to attend the event")
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 }
 
-func getFirstNames(bookings []string) []string {
+func getFirstNames() []string {
 	firstNames := []string{}
-	// iterate over all elements using blank identifier (Ignore index)
 	for _, booking := range bookings {
-		// split the string and return a slice of split values
-		// "Nicole Smith" => ["Nicole", "Smith"]
-		var names = strings.Fields(booking)
-		var firstName = names[0]
-		// append the first name to the slice
-		firstNames = append(firstNames, firstName)
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
 
 func getUserInput() (string, string, string, uint) {
-	// If we do not assign a value to a variable, we need to specify the type
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
-	// Ask user for their input
-	fmt.Println("Please enter your first name: ")
+
+	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
-	fmt.Println("Please enter your last name: ")
+
+	fmt.Println("Enter your last name: ")
 	fmt.Scan(&lastName)
-	fmt.Println("Please enter your email: ")
+
+	fmt.Println("Enter your email address: ")
 	fmt.Scan(&email)
-	fmt.Println("Please enter the number of tickets you would like to purchase: ")
+
+	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
 	return firstName, lastName, email, userTickets
 }
 
-func bookTicket(userTickets uint, bookings []string, firstName string, lastName string, email string, conferenceName string) {
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
-	fmt.Printf(
-		"Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v\n",
-		firstName, lastName, userTickets, email)
-	fmt.Println("There are", remainingTickets, "tickets remaining")
+
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
 
-/*
-
-	//var conferenceName = "Go conference"
-	const conferenceTickets = 50 // value cannot be changed
-	//var remainingTickets = 50
-
-	// alternative way of declaring variables, cannot be used for constants
-	conferenceName := "Go conference"
-	remainingTickets := 50
-
-	//var bookings = [50] string {"Alex", "Steve"} // array of strings with two default values
-
-	// Slices can be used to create dynamic arrays (lists)
-	var bookings [] string
-	// alternative syntax for slice creation
-	bookings := [] string {}
-
-	//fmt.Print("Hello World") // Prints inline
-	//fmt.Println("Hello World") // Prints on new line
-
-	//fmt.Println("Welcome to the", conferenceName, "booking application")
-	// Same as above but with formatting
-	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
-
-	//fmt.Println("There is a total of", conferenceTickets, "tickets,", remainingTickets,
-		//"tickets are still available")
-	// Same as above but with formatting
-	fmt.Printf("There is a total of %v tickets, %v tickets are still available\n", conferenceTickets,
-		remainingTickets)
-
-	fmt.Println("Get your tickets here to attend the event")
-
-	// If we do not assign a value to a variable, we need to specify the type
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
-
-	//firstName = "Denis"
-	//userTickets = 2
-
-	//Pointers
-	fmt.Println(remainingTickets)
-	fmt.Println(&remainingTickets) // Variable containing the memory address of remainingTickets
-
-	//fmt.Println("Hello", firstName, "you have", userTickets, "tickets")
-
-	// Print the types of variables
-	fmt.Printf("conferenceName type is %T and conferenceTickets type is %T\n", conferenceName,
-		conferenceTickets)
-
-	// Ask user for their input
-	fmt.Println("Please enter your first name: ")
-	fmt.Scan(&firstName)
-	fmt.Println("Please enter your last name: ")
-	fmt.Scan(&lastName)
-	fmt.Println("Please enter your email: ")
-	fmt.Scan(&email)
-	fmt.Println("Please enter the number of tickets you would like to purchase: ")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - userTickets
-	//bookings[2]= firstName + " " + lastName
-	bookings = append(bookings, firstName + " " + lastName)
-
-	fmt.Printf(
-		"Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v\n",
-		firstName, lastName, userTickets, email)
-
-	//fmt.Printf("The whole array is %v\n", bookings)
-	//fmt.Printf("Array type is %T\n", bookings)
-	//fmt.Printf("Array length is %v\n", len(bookings))
-
-	fmt.Printf("The whole slice is %v\n", bookings)
-	fmt.Printf("slice type is %T\n", bookings)
-	fmt.Printf("slice length is %v\n", len(bookings))
-
-	fmt.Println("There are", remainingTickets, "tickets remaining")
-
-*/
-
-//----------------------------------------------------------------------------------------------
-// Clean up 1
-//----------------------------------------------------------------------------------------------
-
-/*
-			conferenceName := "Go conference"
-			const conferenceTickets = 50
-			remainingTickets := 50
-			bookings := []string{}
-
-			fmt.Printf("Welcome to the %v booking application\n", conferenceName)
-			fmt.Printf("There is a total of %v tickets, %v tickets are still available\n", conferenceTickets,
-				remainingTickets)
-			fmt.Println("Get your tickets here to attend the event")
-
-			for { // Infinite loop
-				// If we do not assign a value to a variable, we need to specify the type
-				var firstName string
-				var lastName string
-				var email string
-				var userTickets int
-
-				// Ask user for their input
-				fmt.Println("Please enter your first name: ")
-				fmt.Scan(&firstName)
-				fmt.Println("Please enter your last name: ")
-				fmt.Scan(&lastName)
-				fmt.Println("Please enter your email: ")
-				fmt.Scan(&email)
-				fmt.Println("Please enter the number of tickets you would like to purchase: ")
-				fmt.Scan(&userTickets)
-
-				if userTickets > remainingTickets { // If user tries to book more tickets than are available
-					fmt.Println("We only have %v tickets remaining\n", remainingTickets)
-					continue // Go back to the beginning of the loop
-				}
-
-				remainingTickets = remainingTickets - userTickets
-				bookings = append(bookings, firstName+" "+lastName)
-
-				fmt.Printf(
-					"Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v\n",
-					firstName, lastName, userTickets, email)
-
-				firstNames := []string{}
-				// iterate over all elements using blank identifier (Ignore index)
-				for _, booking := range bookings {
-					// split the string and return a slice of split values
-					// "Nicole Smith" => ["Nicole", "Smith"]
-					var names = strings.Fields(booking)
-					var firstName = names[0]
-					// append the first name to the slice
-					firstNames = append(firstNames, firstName)
-
-				}
-				fmt.Println("The first names in the bookings are: %v\n", firstNames)
-				fmt.Println("There are", remainingTickets, "tickets remaining")
-
-				if remainingTickets == 0 {
-					fmt.Println("Sorry, we are sold out")
-					break
-				}
-			}
-
-			fmt.Println("Exiting booking application...")
-
-		//----------------------------------------------------------------------------------------------
-		// Clean up 2
-		//----------------------------------------------------------------------------------------------
-
-		conferenceName := "Go conference"
-		const conferenceTickets = 50
-		remainingTickets := 50
-		bookings := []string{}
-
-		fmt.Printf("Welcome to the %v booking application\n", conferenceName)
-		fmt.Printf("There is a total of %v tickets, %v tickets are still available\n", conferenceTickets,
-			remainingTickets)
-		fmt.Println("Get your tickets here to attend the event")
-
-		for { // Infinite loop
-			// If we do not assign a value to a variable, we need to specify the type
-			var firstName string
-			var lastName string
-			var email string
-			var userTickets int
-
-			// Ask user for their input
-			fmt.Println("Please enter your first name: ")
-			fmt.Scan(&firstName)
-			fmt.Println("Please enter your last name: ")
-			fmt.Scan(&lastName)
-			fmt.Println("Please enter your email: ")
-			fmt.Scan(&email)
-			fmt.Println("Please enter the number of tickets you would like to purchase: ")
-			fmt.Scan(&userTickets)
-
-			if userTickets <= remainingTickets { // If user tries to book more tickets than are available
-				remainingTickets = remainingTickets - userTickets
-				bookings = append(bookings, firstName+" "+lastName)
-
-				fmt.Printf(
-					"Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v\n",
-					firstName, lastName, userTickets, email)
-
-				firstNames := []string{}
-				// iterate over all elements using blank identifier (Ignore index)
-				for _, booking := range bookings {
-					// split the string and return a slice of split values
-					// "Nicole Smith" => ["Nicole", "Smith"]
-					var names = strings.Fields(booking)
-					var firstName = names[0]
-					// append the first name to the slice
-					firstNames = append(firstNames, firstName)
-
-				}
-				fmt.Println("The first names in the bookings are: %v\n", firstNames)
-				fmt.Println("There are", remainingTickets, "tickets remaining")
-
-				if remainingTickets == 0 {
-					fmt.Println("Sorry, we are sold out")
-					break
-				}
-			} else {
-				fmt.Println("We only have %v tickets remaining\n", remainingTickets)
-			}
-		}
-		fmt.Println("Exiting booking application...")
-		// END
-
-
-
-
-	//----------------------------------------------------------------------------------------------
-	// User input validation
-	//----------------------------------------------------------------------------------------------
-
-	conferenceName := "Go conference"
-	const conferenceTickets = 50
-	remainingTickets := 50
-	bookings := []string{}
-
-	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
-	fmt.Printf("There is a total of %v tickets, %v tickets are still available\n", conferenceTickets,
-		remainingTickets)
-	fmt.Println("Get your tickets here to attend the event")
-
-	for { // Infinite loop
-		// If we do not assign a value to a variable, we need to specify the type
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets int
-
-		// Ask user for their input
-		fmt.Println("Please enter your first name: ")
-		fmt.Scan(&firstName)
-		fmt.Println("Please enter your last name: ")
-		fmt.Scan(&lastName)
-		fmt.Println("Please enter your email: ")
-		fmt.Scan(&email)
-		fmt.Println("Please enter the number of tickets you would like to purchase: ")
-		fmt.Scan(&userTickets)
-
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTickets := userTickets > 0 && userTickets <= remainingTickets
-
-		isValidCity := city == "Singapore" || city == "London"
-
-		if userTickets <= remainingTickets { // If user tries to book more tickets than are available
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName)
-
-			fmt.Printf(
-				"Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v\n",
-				firstName, lastName, userTickets, email)
-
-			firstNames := []string{}
-			// iterate over all elements using blank identifier (Ignore index)
-			for _, booking := range bookings {
-				// split the string and return a slice of split values
-				// "Nicole Smith" => ["Nicole", "Smith"]
-				var names = strings.Fields(booking)
-				var firstName = names[0]
-				// append the first name to the slice
-				firstNames = append(firstNames, firstName)
-
-			}
-			fmt.Println("The first names in the bookings are: %v\n", firstNames)
-			fmt.Println("There are", remainingTickets, "tickets remaining")
-
-			if remainingTickets == 0 {
-				fmt.Println("Sorry, we are sold out")
-				break
-			}
-		} else {
-			fmt.Println("We only have %v tickets remaining\n", remainingTickets)
-		}
-	}
-	fmt.Println("Exiting booking application...")
-
-*/
-//----------------------------------------------------------------------------------------------
-// Functions
-//----------------------------------------------------------------------------------------------
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(50 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("#################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
+	fmt.Println("#################")
+	wg.Done()
+}
